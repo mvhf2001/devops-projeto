32ee85e36d48557765b357fb5ecac86a02b0ef4c
mvhf2001 <mvhf2001@hotmail.com>
2023-06-10 19:23:16 -0300

    primeira

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lógica de autenticação com Firebase
+    console.log('Email:', email);
+    console.log('Senha:', password);
+  };
+
+  return (
+    <div className="App">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Senha:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
